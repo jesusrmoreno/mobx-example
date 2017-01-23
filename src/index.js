dccd2ffcf815a8c45ccc8353fecf919dfffea86b
Expero User <expero@Experos-MacBook-Pro.local>
2017-01-23 11:47:56 -0600

    :sparkles: Add basic example of mobx store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import jokeStore from './stores/JokeStore';
+import {useStrict} from 'mobx';
+import {observer} from 'mobx-react';
 import './index.css';
 
+
+// Disallow updating store state outside of actions
+useStrict(true);
+
+const JokeDisplay = ({joke}) => {
+	return (
+		<div>
+			{joke.text}
+		</div>
+	);
+};
+
+// We wrap our app in the observer decorator to be able to
+// 	react to changes in the store
+const App = observer(({jokeStore}) => {
+	return (
+		<div style={{
+			background: '#FEDB5A',
+			width: '100vw',
+			height: '100vh',
+		}}>
+			{jokeStore.jokesAsArray.map(joke => {
+				return <JokeDisplay key={joke.id} joke={joke} />;
+			})}
+		</div>
+	);
+});
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<App jokeStore={jokeStore}/>,
+	document.getElementById('root')
 );
---
+++ src/stores/JokeStore.js
@@ -0,0 +1,112 @@
+import {extendObservable, computed, action, autorun} from 'mobx';
+import uniqueId from 'lodash/uniqueId';
+import omit from 'lodash/omit';
+
+function joke(text) {
+	return {
+		id: uniqueId('joke_'),
+		text,
+		createdAt: Date.now(),
+		likes: 0,
+		dislikes: 0,
+	};
+}
+
+class JokeStore {
+	constructor() {
+		const firstJoke = joke('Why did the chicken cross the road?');
+		// We use extendObservable to set which properties of our model should 
+		// 	be reactive. When these properties change, mobx will trigger 
+		//  reactions e.g: rerender things that depend on them, computed 
+		//  properties, autoruns we've added
+		extendObservable(this, {
+			currentJokeIndex: 0,
+			hasPrevious: 0,
+			hasNext: 0,
+			jokesById: {
+				[firstJoke.id]: firstJoke,
+			},
+			popularLimit: 2,
+
+			// This is an example of a computed property, 
+			// 	When the dependencies update (in this case jokesById) and only 
+			// 	when deps update, this function will be called to reflect the 
+			//	changes.
+			popularJokes: computed(() => {
+				const jokes = this.jokesAsArray;
+				return jokes.filter(joke => joke.likes > this.popularLimit);
+			}),
+
+
+			// This will be run when we add a new joke to our jokesById obj
+			jokesAsArray: computed(() => {
+				return Object.keys(this.jokesById).map(
+					id => this.jokesById[id]
+				);
+			}),
+		});
+
+		autorun(() => {
+			console.log(this.jokesAsArray.slice());
+		})
+	}
+
+	// This is an example of an action 
+	// actions are used to modify state.
+	addJokeAction = action((text) => {
+		const newJoke = joke(text);
+
+		// this.jokesById[newJoke.id] = newJoke;
+		// this is common mistake: mobx by default only tracks existing 
+		// 	properties for objects. 
+		//  https://mobx.js.org/best/react.html
+
+		// Proper way to update objects, or we can use a mobx map / object
+		// 	https://mobx.js.org/refguide/object.html
+		this.jokesById = {
+			...this.jokesById,
+			[newJoke.id]: newJoke,
+		};
+
+	})
+
+	// If we use mobx in non-strict mode we can directly modify state
+	// without having to wrap it in an action. 
+	// addJoke = (text) => {
+		// const newJoke = joke(text);
+
+		// this.jokesById[newJoke.id] = newJoke;
+		// this is common mistake: mobx by default only tracks existing 
+		// 	properties for objects. 
+		//  https://mobx.js.org/best/react.html
+
+		// Proper way to update objects, or we can use a mobx map / object
+		// 	https://mobx.js.org/refguide/object.html
+		// this.jokesById = {
+		// 	...this.jokesById,
+		// 	[newJoke.id]: newJoke,
+		// };
+	// }
+
+	deleteJoke = action((id) => {
+		this.jokesById = omit(this.jokesById, id);
+	});
+
+	likeJoke = action((id) => {
+		this.jokesById[id].likes++;
+	});
+
+	dislikeJoke = action((id) => {
+		this.jokesById[id].dislikes++;
+	});
+
+	editJoke = action((id, newText) => {
+		this.jokesById[id].text = newText;
+	});
+}
+
+export default new JokeStore();
+
+export {
+	JokeStore
+}
