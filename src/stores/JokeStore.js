c3a8d433ef292412ba3157e3117358a75779e127
Jesus Moreno <jesus.moreno@experoinc.com>
2017-01-23 11:52:12 -0600

    :art: make spacing consistent

--- src/stores/JokeStore.js
+++ src/stores/JokeStore.js
@@ -16,9 +16,9 @@ class JokeStore {
 	constructor() {
 		const firstJoke = joke('Why did the chicken cross the road?');
 		// We use extendObservable to set which properties of our model should 
-		//	be reactive. When these properties change, mobx will trigger 
-		//	reactions e.g: rerender things that depend on them, computed 
-		//	properties, autoruns we've added
+		//  be reactive. When these properties change, mobx will trigger 
+		//  reactions e.g: rerender things that depend on them, computed 
+		//  properties, autoruns we've added
 		extendObservable(this, {
 			currentJokeIndex: 0,
 			hasPrevious: 0,
@@ -29,9 +29,9 @@ class JokeStore {
 			popularLimit: 2,
 
 			// This is an example of a computed property, 
-			//	When the dependencies update (in this case jokesById) and only 
-			//	when deps update, this function will be called to reflect the 
-			//	changes.
+			//  When the dependencies update (in this case jokesById) and only 
+			//  when deps update, this function will be called to reflect the 
+			//  changes.
 			popularJokes: computed(() => {
 				const jokes = this.jokesAsArray;
 				return jokes.filter(joke => joke.likes > this.popularLimit);
@@ -60,11 +60,11 @@ class JokeStore {
 
 		// this.jokesById[newJoke.id] = newJoke;
 		// this is common mistake: mobx by default only tracks existing 
-		// 	properties for objects. 
+		//  properties for objects. 
 		//  https://mobx.js.org/best/react.html
 
 		// Proper way to update objects, or we can use a mobx map / object
-		// 	https://mobx.js.org/refguide/object.html
+		//  https://mobx.js.org/refguide/object.html
 		this.jokesById = {
 			...this.jokesById,
 			[newJoke.id]: newJoke,
@@ -79,14 +79,14 @@ class JokeStore {
 
 		// this.jokesById[newJoke.id] = newJoke;
 		// this is common mistake: mobx by default only tracks existing 
-		// 	properties for objects. 
+		//  properties for objects. 
 		//  https://mobx.js.org/best/react.html
 
 		// Proper way to update objects, or we can use a mobx map / object
-		// 	https://mobx.js.org/refguide/object.html
+		//  https://mobx.js.org/refguide/object.html
 		// this.jokesById = {
-		// 	...this.jokesById,
-		// 	[newJoke.id]: newJoke,
+		//  ...this.jokesById,
+		//  [newJoke.id]: newJoke,
 		// };
 	// }
 
